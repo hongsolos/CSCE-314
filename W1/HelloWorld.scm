2f7c97e7ea0a3001a7f3ef7abdc6647f2b1e0ff2
Han Hong <hongichhan@compute.cs.tamu.edu>
2018-08-31 10:45:56 -0500

    First practice added

---
+++ W1/HelloWorld.scm
@@ -0,0 +1,14 @@
+;File: HelloWorld.scm
+;Author: Han Hong
+;Date: 20180831
+;Description: First Scheme Program
+
+(define (square n)
+  ;return square of a number
+  (* n n)
+)
+
+(display "Hello World!\n")
+(display "My first addition.\n")
+(display (square 2))
+(exit)
